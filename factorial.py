5b0c92e561b985b6efec68ea41b9edacbe2fd048
Ocinom <64297978+Ocinom@users.noreply.github.com>
2020-10-24 22:19:22 +1100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,19 @@
-def fact(n):
-    if n == 0:
-        return 1
-    if n == 1:
-        return n
-    if n > 1:
-        return n * fact(n - 1)
+# Recursive. Pretty bad ngl. DP/iterative/tail recursion?
 
-print(fact(5))
+#def fact(n):
+#    if n == 0:
+#        return 1
+#    if n == 1:
+#        return n
+#    if n > 1:
+#        return n * fact(n - 1)
+
+#print(fact(5))
+
+def fact(n):
+    res = 1
+    if n == 0 or n == 1:
+        return 1
+    for i in range(1, n + 1):
+        res *= i
+       return res
